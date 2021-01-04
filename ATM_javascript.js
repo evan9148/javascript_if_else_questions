41f98226fa2bbd4b1b0db466fdb7754e255527c9
Evanjaline <66587868+evan9148@users.noreply.github.com>
2021-01-04 11:18:25 +0530

    Update ATM_javascript.js

--- ATM_javascript.js
+++ ATM_javascript.js
@@ -4,29 +4,25 @@ var account = readlineSync.question("enter your account")
 var pin = readlineSync.question("enter your pin")
 var amount = readlineSync.question("enter your amount")
 if (language == "english" || language == "hindi"){
-    console.log(language,"language is accpected");
-}
-    if (account == "saving" || language == "current"){
-        console.log(account,"account can be accpected");
-    }
-        if (pin == 7766){
-            console.log(pin,"your pin can excute");
-        }
+    console.log("language accpected",language)
+    if (account == "current" || account == "saving"){
+        console.log("account accpected",account)
+        if (pin == 7785){
+            console.log("your pin valid",pin)
             if (amount<=50000){
-                console.log(amount,"you can withdraw money");
+                console.log("you can withdraw your money",amount)
             }else{
-                console.log(amount,"you can not withdraw your account as only 50000")
-            }else {
-                console.log(pin,"your pin invalid")
-        }else {
-            console.log(account,"your account is invalid")
-    }else {
-        console.log(language,"your language is invalid")
+                console.log("you don't sufficient money on your ATM",amount)
+            }
+        }else{
+            console.log("your pin is invalid",pin)
+        }
+    }else{
+        console.log("your account not accpected",account)
+    }
+}else{
+    console.log("your languange not accpected",language)
 }
 
 
-
-    
-    
-    
     
